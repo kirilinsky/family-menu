6cf0e4b50d53b1bc892c724f3553527c3cf31bc7
kirilinsky <kirill.ilinsky@gmail.com>
2026-04-23 15:29:35 +0200

    fix: remove unused imports

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,15 +1,13 @@
-import { Calendar, CalendarDays, createTheme } from "react-calendar-datetime";
 import Order from "../Order";
 import QR from "../Qr";
 import "./sidebar.scss";
 import CalendarWrapper from "../CalendarWrapper";
 
-
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <Order />
-      <CalendarWrapper/>  
+      <CalendarWrapper />
       <QR />
     </div>
   );
