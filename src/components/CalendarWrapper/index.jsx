987a8c582913151e755013c9c8759eaca9a02643
kirilinsky <kirill.ilinsky@gmail.com>
2026-04-25 19:52:48 +0200

    feat: update calendar

--- src/components/CalendarWrapper/index.jsx
+++ src/components/CalendarWrapper/index.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import { CalendarNav, CalendarDays } from "react-calendar-datetime/modules";
 import { Calendar, createTheme } from "react-calendar-datetime";
 
-const orderTheme = createTheme(
-  {
-    accent: "#fefefe",
-    backdrop: "#ffffff",
-    highlight: "#523637",
-    tone: "#f4f4f5",
-    text: "#18181b",
-    stroke: "#e4e4e7",
-    shadow: "rgba(24, 24, 27, 0.08)",
-    disabled: "#a1a1aa",
-    weekend: "#7e4446",
-    range: "#f4f4f5",
-  },
-  "light",
-);
+const orderTheme = createTheme({
+  accent: "#fefefe",
+  backdrop: "#ffffff",
+  highlight: "#523637",
+  tone: "#f4f4f5",
+  text: "#18181b",
+  stroke: "#e4e4e7",
+  shadow: "rgba(24, 24, 27, 0.08)",
+  disabled: "#a1a1aa",
+  weekend: "#7e4446",
+  range: "#f4f4f5",
+});
 
 const CalendarWrapper = () => {
   return (
