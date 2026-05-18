5853ac5a85c0df83616f65dd6e56d91834795633
kirilinsky <kirill.ilinsky@gmail.com>
2026-05-18 09:32:20 +0200

    feat: migrate to vite

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import { Provider } from "jotai";
+import App from "./App";
+import "./index.scss";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+);
